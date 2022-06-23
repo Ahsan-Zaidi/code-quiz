76134a088657eed7f8da32d33be76d365bdb852c
Zaidi <zaidiahsan.r@gmail.com>
2022-06-23 15:31:30 -0500

    penalizing and rewarding players

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,27 +32,6 @@ var currentQuestion = -1;
 var timeRem = 0;
 var timer;
 
-//creating html elements through Javascript setting up the initial page layout.
-var quizBody = document.createElement('div');
-quizBody.setAttribute("id", "quizBody");
-document.querySelector("#quiz").append(quizBody);
-var h1 = document.createElement('h1');
-h1.textContent = "Coding Quiz";
-quizBody.append(h1);
-var p = document.createElement('p');
-p.textContent = "Javascript code quiz! Answer quick... Time is of the essance!";
-quizBody.append(p);
-
-//creating the start button and appending it to Body
-var startBtn = document.createElement("Button");
-startBtn.textContent = "Start Quiz";
-quizBody.append(startBtn);
-
-//adding event listener to button so that once it is clicked the start function will begin
-startBtn.addEventListener("click", function (){
-    start();
-});
-
 //function to start the timer 
 function start () {
     //clears the innerHtml to present questions
@@ -75,6 +54,41 @@ function start () {
     },1000)
     //if above "if" statment is not fufilled the function 'next' will be executed presenting the next question
     next()
+}
+
+//creating html elements through Javascript setting up the initial page layout.
+var quizBody = document.createElement('div');
+quizBody.setAttribute("id", "quizBody");
+document.querySelector("#quiz").append(quizBody);
+var h1 = document.createElement('h1');
+h1.textContent = "Coding Quiz";
+quizBody.append(h1);
+var p = document.createElement('p');
+p.textContent = "Javascript code quiz! Answer quick... Time is of the essance!";
+quizBody.append(p);
+
+//creating the start button and appending it to Body
+var startBtn = document.createElement("Button");
+startBtn.textContent = "Start Quiz";
+quizBody.append(startBtn);
+
+//adding event listener to button so that once it is clicked the start function will begin
+startBtn.addEventListener("click", function (){
+    start();
+});
+
+//function to penalize player for getting a question wrong
+function incorrect() {
+    timeLeft -= 15;
+    document.getElementById("quizBody").innerHTML = " ";
+    next();
+}
+
+//function to reward player for getting the question correct
+function correct() {
+    score += 20;
+    document.getElementById("quizBody").innerHTML = " ";
+    next();
 }
 
 //create function to iterate through the questions array
